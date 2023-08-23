e0adf4692e5a19f95c4db21d10f414c72736114c
kevin <bettkevin965@gmail.com>
2023-08-23 14:23:17 +0300

    Complete assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,41 @@
-import React, { useState } from "react";
-import Question from "./Question";
-import quiz from "../data/quiz";
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import Question from './Question'; 
+import quiz from '../data/quiz';
 
 function App() {
   const [questions, setQuestions] = useState(quiz);
   const [currentQuestionId, setCurrentQuestion] = useState(1);
   const [score, setScore] = useState(0);
   const currentQuestion = questions.find((q) => q.id === currentQuestionId);
+  const [timeRemaining, setTimeRemaining] = useState(10); 
+
+  useEffect(() => {
+    let timer;
+
+    const handleTimer = () => {
+      if (timeRemaining > 0) {
+        setTimeRemaining(timeRemaining - 1);
+      } else {
+        setTimeRemaining(10);
+        handleQuestionAnswered(false);
+      }
+    };
+    timer = setInterval(handleTimer, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [timeRemaining, currentQuestionId]);
 
   function handleQuestionAnswered(correct) {
     if (currentQuestionId < questions.length) {
-      setCurrentQuestion((currentQuestionId) => currentQuestionId + 1);
+      setCurrentQuestion(currentQuestionId + 1);
     } else {
       setCurrentQuestion(null);
     }
     if (correct) {
-      setScore((score) => score + 1);
+      setScore(score + 1);
     }
   }
 
@@ -23,16 +43,14 @@ function App() {
     <main>
       <section>
         {currentQuestion ? (
-          <Question
-            question={currentQuestion}
-            onAnswered={handleQuestionAnswered}
-          />
+          <Question question={currentQuestion} onAnswered={handleQuestionAnswered} />
         ) : (
           <>
             <h1>Game Over</h1>
             <h2>Total Correct: {score}</h2>
           </>
         )}
+        <p>{timeRemaining} seconds remaining</p> {/* Display the timer */}
       </section>
     </main>
   );
